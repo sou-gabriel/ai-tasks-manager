6f842c9a6ccaca3ddabcf2aabf0d4a0d9bc8b404
Gabriel Ramos <dev.gabrielramos@gmail.com>
2024-12-21 01:07:49 -0300

    Update getSuggestions function to use environment variable for API base URL

--- src/modules/ai/services/get-suggestions.ts
+++ src/modules/ai/services/get-suggestions.ts
@@ -1,5 +1,8 @@
+import { env } from "@/env";
+
 export async function getSuggestions() {
-  const res = await fetch("http://localhost:3000/api/chat/suggestions");
+  const url = `${env.NEXT_PUBLIC_API_BASE_URL}/api/chat/suggestions`
+  const res = await fetch(url);
   const suggestions: string = await res.json();
 
   return suggestions as unknown as string[];
