1402386dc97cdbf1e987e85ded0df546b3feab4f
Gabriel Ramos <dev.gabrielramos@gmail.com>
2024-12-20 22:47:32 -0300

    Update API URL in getTasks function to use NEXT_PUBLIC_API_BASE_URL

--- src/modules/tasks/services/get-services.ts
+++ src/modules/tasks/services/get-services.ts
@@ -2,7 +2,7 @@ import { env } from '@/env'
 import { tasksModel } from './schema'
 
 export async function getTasks(query?: string | null) {
-  const url = `${env}/api/tasks?q=${query || ''}`
+  const url = `${env.NEXT_PUBLIC_API_BASE_URL}/api/tasks?q=${query || ''}`
   const res = await fetch(url)
 
   const tasks = await res.json()
