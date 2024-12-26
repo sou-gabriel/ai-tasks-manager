4fe2c55dddc2b8d878813a451e71a83e6f8459cd
Gabriel Ramos <dev.gabrielramos@gmail.com>
2024-12-25 23:21:04 -0300

    Add HttpClient utility for API requests with error handling

---
+++ src/http/client.ts
@@ -0,0 +1,34 @@
+import { env } from "@/env";
+
+class HttpClientError extends Error {
+  status: number;
+
+  constructor(message: string, status: number) {
+    super(message);
+
+    this.status = status;
+    this.name = "HttpClientError";
+  }
+}
+
+export async function httpClient<T = unknown>(
+  path: string,
+  options: RequestInit = {},
+) {
+  const url = new URL(path, env.NEXT_PUBLIC_API_BASE_URL);
+
+  const res = await fetch(url, options);
+
+  const contentType = res.headers.get("Content-Type");
+  const isJson = contentType?.includes("application/json");
+
+  if (res.ok) {
+    return isJson ? (res.json() as Promise<T>) : undefined;
+  }
+
+  const errorMessage = isJson
+    ? (await res.json()).message
+    : `Failed to fetch "${path}"`;
+
+  throw new HttpClientError(errorMessage, res.status);
+}
