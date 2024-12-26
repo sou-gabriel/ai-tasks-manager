27750175530082fd434683660d3227af68903fcb
Gabriel Ramos <dev.gabrielramos@gmail.com>
2024-12-25 23:21:23 -0300

    Enhance task fetching with error handling and refetch capability

--- src/modules/tasks/services/get-services.ts
+++ src/modules/tasks/services/get-services.ts
@@ -1,10 +1,7 @@
-import { env } from '@/env'
-import { tasksModel } from './schema'
+import { httpClient } from "@/http/client";
+import { tasksModel } from "./schema";
 
 export async function getTasks(query?: string | null) {
-  const url = `${env.NEXT_PUBLIC_API_BASE_URL}/api/tasks?q=${query || ''}`
-  const res = await fetch(url)
-
-  const tasks = await res.json()
-  return tasksModel.parse(tasks)
+  const tasks = await httpClient(`/api/tasks?q=${query || ""}`);
+  return tasksModel.parse(tasks);
 }
